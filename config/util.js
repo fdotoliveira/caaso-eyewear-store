6e57e92b1c1cd612166fa5c3142b3792ae6640ea
Matheus Giraldi Alvarenga <62122159+matheusgiraldi@users.noreply.github.com>
2023-07-09 19:18:25 -0300

    Delete config directory

--- config/util.js
+++
@@ -1,8 +0,0 @@
-exports.isEmpty = (val) => {
-    if (val === undefined || val === null || val === "undefined") {
-        return true;
-    } else if (val === "") {
-        return true;
-    }
-    return false;
-};
